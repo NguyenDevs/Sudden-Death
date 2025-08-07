842406077c8712a3beff2a518967fe669bcdf9a7
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2025-08-07 12:03:10 +0700

    Remove unused code

--- src/main/java/org/nguyendevs/suddendeath/world/StatusRetriever.java
+++ src/main/java/org/nguyendevs/suddendeath/world/StatusRetriever.java
@@ -2,14 +2,6 @@ package org.nguyendevs.suddendeath.world;
 
 import org.nguyendevs.suddendeath.manager.EventManager.WorldStatus;
 
-/**
- * Interface for retrieving the status of a world event in the SuddenDeath plugin.
- */
 public interface StatusRetriever {
-	/**
-	 * Retrieves the current world status associated with this event handler.
-	 *
-	 * @return The WorldStatus enum value representing the event's status.
-	 */
 	WorldStatus getStatus();
 }
