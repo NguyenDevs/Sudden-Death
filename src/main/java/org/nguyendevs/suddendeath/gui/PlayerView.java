acd172f75e0401a6626a4a60bee8704df0ef78ba
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2025-11-21 20:07:14 +0700

    Update PlayerView.java

--- src/main/java/org/nguyendevs/suddendeath/gui/PlayerView.java
+++ src/main/java/org/nguyendevs/suddendeath/gui/PlayerView.java
@@ -92,7 +92,7 @@ public class PlayerView extends PluginInventory {
     );
     private static final EnumSet<Feature> MOB_SET = EnumSet.of(
             Feature.ABYSSAL_VORTEX,
-            Feature.AIM_BOT,
+            //Feature.AIM_BOT,
             Feature.ANGRY_SPIDERS,
             Feature.BONE_GRENADES,
             Feature.BONE_WIZARDS,
@@ -346,7 +346,7 @@ public class PlayerView extends PluginInventory {
     private Material getVisualMaterial(Feature f) {
         switch (f) {
             case ABYSSAL_VORTEX: return Material.GUARDIAN_SPAWN_EGG;
-            case AIM_BOT: return Material.SPECTRAL_ARROW;
+            //case AIM_BOT: return Material.SPECTRAL_ARROW;
             case ANGRY_SPIDERS: return Material.SPIDER_SPAWN_EGG;
             case BLOOD_MOON: return Material.ZOMBIE_HEAD;
             case BONE_GRENADES: return Material.SKELETON_SPAWN_EGG;
