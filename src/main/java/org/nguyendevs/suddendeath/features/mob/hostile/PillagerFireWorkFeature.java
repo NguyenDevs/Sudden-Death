39a17e12ebc92f95f64727ed0ed27f7db1aa0835
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2025-12-10 16:41:02 +0700

    Rework Firework Arrow listener

--- src/main/java/org/nguyendevs/suddendeath/features/mob/hostile/PillagerFireWorkFeature.java
+++ src/main/java/org/nguyendevs/suddendeath/features/mob/hostile/PillagerFireWorkFeature.java
@@ -4,12 +4,14 @@ import org.bukkit.*;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.FireworkExplodeEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 import org.nguyendevs.suddendeath.features.base.AbstractFeature;
 import org.nguyendevs.suddendeath.util.Feature;
 import org.nguyendevs.suddendeath.util.Utils;
@@ -36,7 +38,34 @@ public class PillagerFireWorkFeature extends AbstractFeature {
             double chance = Feature.FIREWORK_ARROWS.getDouble("chance-percent") / 100.0;
             if (RANDOM.nextDouble() > chance) return;
 
-            arrow.setMetadata(FIREWORK_ARROW_KEY, new FixedMetadataValue(plugin, true));
+            Entity originalArrow = event.getProjectile();
+            Vector velocity = originalArrow.getVelocity();
+            Location spawnLoc = originalArrow.getLocation();
+
+            Firework firework = pillager.getWorld().spawn(spawnLoc, Firework.class);
+            FireworkMeta meta = firework.getFireworkMeta();
+
+            Color brown = Color.fromRGB(160,82,45);
+            Color maroon = Color.MAROON;
+
+            FireworkEffect effect = FireworkEffect.builder()
+                    .with(FireworkEffect.Type.BALL)
+                    .withColor(brown, maroon)
+                    .withFade(Color.BLACK)
+                    .trail(true)
+                    .flicker(true)
+                    .build();
+            meta.addEffect(effect);
+            meta.setPower(2);
+            firework.setFireworkMeta(meta);
+
+            firework.setVelocity(velocity.multiply(1.2));
+            firework.setShooter(pillager);
+
+            firework.setMetadata(FIREWORK_ARROW_KEY, new FixedMetadataValue(plugin, true));
+
+            event.setProjectile(firework);
+
             pillager.getWorld().playSound(pillager.getLocation(), Sound.ENTITY_FIREWORK_ROCKET_LAUNCH, 1.0f, 1.0f);
 
             new BukkitRunnable(){
@@ -55,62 +84,34 @@ public class PillagerFireWorkFeature extends AbstractFeature {
     }
 
     @EventHandler
-    public void onProjectileHit(ProjectileHitEvent event){
-        if(!(event.getEntity() instanceof Arrow arrow)) return;
-        if(!arrow.hasMetadata(FIREWORK_ARROW_KEY)) return;
-
-        try{
-            Location loc = arrow.getLocation();
-
-            explode(loc);
-            arrow.remove();
-        } catch (Exception e){
-            plugin.getLogger().log(Level.WARNING, "Error in PillagerFireworkFeature hit event", e);
+    public void onFireworkExplode(FireworkExplodeEvent event){
+        if(event.getEntity().hasMetadata(FIREWORK_ARROW_KEY)){
+            handleExplosion(event.getEntity());
         }
     }
 
-    private void explode(Location loc){
-        World world = loc.getWorld();
-        if(world == null) return;
+    private void handleExplosion(Firework firework){
+        try{
+            Location loc = firework.getLocation();
+            double range = Feature.FIREWORK_ARROWS.getDouble("area");
+            double damage = Feature.FIREWORK_ARROWS.getDouble("damage");
+            double duration = Feature.FIREWORK_ARROWS.getDouble("duration");
 
-        double range = Feature.FIREWORK_ARROWS.getDouble("area");
-        double damage = Feature.FIREWORK_ARROWS.getDouble("damage");
-        double duration = Feature.FIREWORK_ARROWS.getDouble("duration");
+            loc.getWorld().playSound(loc, Sound.ENTITY_GENERIC_EXPLODE, 2.0f, 0.8f);
+            loc.getWorld().playSound(loc, Sound.ENTITY_FIREWORK_ROCKET_BLAST, 1.5f, 1.0f);
 
-        Firework fw = (Firework) world.spawnEntity(loc, EntityType.FIREWORK);
-        FireworkMeta meta = fw.getFireworkMeta();
-        meta.addEffect(FireworkEffect.builder()
-                .with(FireworkEffect.Type.BALL)
-                .withColor(Color.fromRGB(139,69,19), Color.MAROON, Color.ORANGE)
-                .withFade(Color.BLACK)
-                .trail(true)
-                .flicker(true)
-                .build());
-        FireworkMeta meta2 = fw.getFireworkMeta();
-        meta2.addEffect(FireworkEffect.builder()
-                .with(FireworkEffect.Type.BALL_LARGE)
-                .withColor(Color.fromRGB(139,69,19), Color.ORANGE, Color.PURPLE)
-                .withFade(Color.GRAY)
-                .trail(true)
-                .flicker(true)
-                .build());
-        meta.setPower(0);
-        fw.setFireworkMeta(meta);
-        fw.detonate();
-        meta2.setPower(0);
-        fw.setFireworkMeta(meta2);
-        fw.detonate();
+            for(Entity entity : loc.getWorld().getNearbyEntities(loc, range, range, range)){
+                if(entity instanceof Player player && !Utils.hasCreativeGameMode(player)){
+                    Utils.damage(player, damage, true);
 
-        world.playSound(loc, Sound.ENTITY_GENERIC_EXPLODE, 2.0f, 0.8f);
-        world.playSound(loc, Sound.ENTITY_FIREWORK_ROCKET_BLAST, 1.5f, 1.0f);
-
-        for(Entity entity : world.getNearbyEntities(loc, range, range, range)){
-            if(entity instanceof Player player && !Utils.hasCreativeGameMode(player)){
-                Utils.damage(player, damage, true);
-
-                player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, (int)(duration * 20), 1));
-                player.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS, 4, 1));
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, (int)(duration * 20), 1));
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS, 4, 1));
+                player.playHurtAnimation(0);
+                }
             }
+
+        } catch (Exception e){
+            plugin.getLogger().log(Level.WARNING, "Error in PillagerFireworkFeature explode event", e);
         }
     }
 }
