8dd807609e75a2188ea045f5f63104de8b2e4a90
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2026-03-01 00:02:03 +0700

    Update BloodMoon.java

--- src/main/java/org/nguyendevs/suddendeath/Features/world/BloodMoon.java
+++ src/main/java/org/nguyendevs/suddendeath/Features/world/BloodMoon.java
@@ -212,6 +212,14 @@ public class BloodMoon extends WorldEventHandler {
 					.isFlagAllowedAtLocation(event.getLocation(), CustomFlag.SDS_EVENT))
 				return;
 
+			// Iterate backwards to safely remove elements while iterating
+			for (int i = event.blockList().size() - 1; i >= 0; i--) {
+				Block b = event.blockList().get(i);
+				if (SuddenDeath.getInstance().getClaimManager().isClaimed(b.getLocation())) {
+					event.blockList().remove(i);
+				}
+			}
+
 			final List<Block> blocks = new ArrayList<>(event.blockList());
 			if (blocks.isEmpty())
 				return;
@@ -236,6 +244,7 @@ public class BloodMoon extends WorldEventHandler {
 					for (int y = fy0; y <= fy1; y++) {
 						for (int z = fz0; z <= fz1; z++) {
 							Block block = world.getBlockAt(x, y, z);
+							// Also check claims during the delayed post-explosion crater modification
 							if (SuddenDeath.getInstance().getClaimManager().isClaimed(block.getLocation())) {
 								continue;
 							}
